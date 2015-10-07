4ab84e218a2f9c0ab2d0124fa12f1ccd9995e661
Duncan M. McGreggor <oubiwann@gmail.com>
2015-10-06 22:18:56 -0500

    Added SR placeholders.

---
+++ src/lcmap_rest/surface_reflectance.clj
@@ -0,0 +1,16 @@
+(ns lcmap-rest.surface-reflectance
+  (:require [clojurewerkz.cassaforte.client :as cc]
+            [clojurewerkz.cassaforte.cql :as cql]
+            [clojurewerkz.cassaforte.query :refer :all]))
+
+(defn get-resource-children []
+  ""
+  [])
+
+(defn get-tiles []
+  ""
+  [])
+
+(defn get-rod []
+  ""
+  [])
