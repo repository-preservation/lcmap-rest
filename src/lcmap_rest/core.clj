d76ac9935ee24c0df50b3bbdd85dac5584306cdb
Duncan McGreggor <oubiwann@gmail.com>
2015-10-09 13:46:32 -0500

    Updated requires.

--- src/lcmap_rest/core.clj
+++ src/lcmap_rest/core.clj
@@ -1,6 +1,9 @@
 ;;;; This module is for top-level  code independent of both the routes and main
 ;;;; modules, providing a means of averting cyclic dependencies.
 (ns lcmap-rest.core
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [lcmap-rest.util :as util]
+            [lcmap-rest.l8 :as l8]
+            [lcmap-rest.l8.surface-reflectance]))
 
 (def noop :noop)
