17596f45cbd39756d6854a28c3e76ec96d55b722
Duncan McGreggor <oubiwann@gmail.com>
2015-10-09 08:48:07 -0500

    Added contexts for modules.

---
+++ src/lcmap_rest/core.clj
@@ -0,0 +1,5 @@
+(ns lcmap-rest.core
+  (:require [clojure.tools.logging :as log]
+            [org.httpkit.server :as httpkit]))
+
+(def context "/api")
---
+++ src/lcmap_rest/l8/core.clj
@@ -0,0 +1,4 @@
+(ns lcmap-rest.l8.core
+  (:require [lcmap-rest.core :as lcmap]))
+
+(def context (str lcmap/context "/L1/T/Landsat/8"))
