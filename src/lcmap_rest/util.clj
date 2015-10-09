46143f75d138a5b84bf9b18894ad0ce6d5df3c77
Duncan McGreggor <oubiwann@gmail.com>
2015-10-09 15:42:07 -0500

    Added utility function for parsing version from Accept.

--- src/lcmap_rest/util.clj
+++ src/lcmap_rest/util.clj
@@ -9,3 +9,12 @@
     {:media-range mr
      :quality  (java.lang.Double. (or q "1"))
      :accept-extension ae}))
+
+(def accept-version-regex (re-pattern #"application/vnd\.(usgs\.lcmap.)(v(\d(\.\d)?))\+([^;]+)"))
+
+(defn parse-accept-version [str]
+  (let [[_ vend str-vers vers _ ct] (re-find accept-version-regex str)]
+    {:vendor vend
+     :string-version str-vers
+     :version  (java.lang.Double. (or vers "1"))
+     :content-type ct}))
