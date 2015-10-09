66965c032ab8eeb13b186726ff211f26c04c1f1c
Duncan McGreggor <oubiwann@gmail.com>
2015-10-09 13:47:05 -0500

    More clean ups:  * ditched record  * pre-compiled regex  * used short-form for coerce  * changed (is ...) to (are ...) in test

--- src/lcmap_rest/util.clj
+++ src/lcmap_rest/util.clj
@@ -1,11 +1,11 @@
 (ns lcmap-rest.util)
 
-(defrecord Accept [media-range quality accept-extension])
+(def accept-regex (re-pattern #"([^;]+)\s*(?:;q=([0-9+\.]+))?\s*(;.+)*"))
 
 (defn parse-accept [str]
   "Parse a single accept string into a map"
   ; according to RFC2616, the "q" parameter must precede the accept-extension
-  (let [pattern #"([^;]+)\s*(?:;q=([0-9+\.]+))?\s*(;.+)*"
-        [_ media-range qvalue accept-extension :as matches] (re-find pattern str)
-        quality (java.lang.Double/parseDouble (or qvalue "1"))]
-    (Accept. media-range quality accept-extension)))
+  (let [[_ mr q ae] (re-find accept-regex str)]
+    {:media-range mr
+     :quality  (java.lang.Double. (or q "1"))
+     :accept-extension ae}))
--- test/lcmap_rest/util_test.clj
+++ test/lcmap_rest/util_test.clj
@@ -3,7 +3,10 @@
             [lcmap-rest.util :as util]))
 
 (deftest parse-accept-test
-  (is (= (util/parse-accept "text/html")
-         (util/map->Accept {:media-range "text/html" :quality 1.0 :accept-extension nil} )))
-  (is (= (util/parse-accept "text/html;q=0.9")
-         (util/map->Accept {:media-range "text/html" :quality 0.9 :accept-extension nil} ))))
+  (are [a b] (= a b)
+    (util/parse-accept "text/html") {:accept-extension nil
+                                     :media-range "text/html"
+                                     :quality 1.0}
+    (util/parse-accept "text/html;q=0.9") {:media-range "text/html"
+                                           :quality 0.9
+                                           :accept-extension nil}))
