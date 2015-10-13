a7f4cc726e5bbec9e16647996ac4215465cd88cb
Duncan M. McGreggor <oubiwann@gmail.com>
2015-10-12 19:20:47 -0500

    Renamed main to app and moved app logic from routes.

---
+++ src/lcmap_rest/app.clj
@@ -0,0 +1,47 @@
+(ns lcmap-rest.app
+  (:require [clojure.tools.logging :as log]
+            [org.httpkit.server :as httpkit]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [compojure.response :as response]
+            [lcmap-rest.routes :as routes]
+            [lcmap-rest.util :as util]))
+  (:gen-class))
+
+(defn get-api-version [version default]
+  (cond
+    (and (>= version 0.0) (< version 1.0)) #'routes/v0
+    (and (>= version 1.0) (< version 2.0)) #'routes/v1
+    (and (>= version 2.0) (< version 3.0)) #'routes/v2
+    :else default))
+
+(defn version-handler
+  "This is a custom handler "
+  [default-api]
+  (fn [request]
+    (let [headers (:headers request)
+          ;; This next line is nuts and took a while to figure out -- results
+          ;; are rendered in log files as symbols, but (headers 'accept) and
+          ;; ('accept headers) didn't work. After an inordinate amount of trial
+          ;; and error, it was discovered that the header keys are actually in
+          ;; lower-case strings at this point in the middleware chain.
+          accept (headers "accept")
+          {version :version} (util/parse-accept-version accept)
+          api (get-api-version version default-api)]
+      (response/render (api request) request))))
+
+(defroutes app
+  (-> default-api-version
+      version-handler
+      ;; XXX once we support SSL, api-defaults needs to be changed to
+      ;; secure-api-defaults
+      (wrap-defaults api-defaults)
+      (wrap-json-response)))
+
+(defn -main
+  "This is the entry point.
+
+  'lein run' will use this as well as 'java -jar'."
+  [& args]
+  (log/info "Starting Compojure server ...")
+  (httpkit/run-server #'app {:port 8080}))
--- src/lcmap_rest/routes.clj
+++ src/lcmap_rest/routes.clj
@@ -1,9 +1,6 @@
 (ns lcmap-rest.routes
   (:require [clojure.tools.logging :as log]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [ring.middleware.json :refer [wrap-json-response]]
             [compojure.core :refer [GET context defroutes]]
-            [compojure.response :as response]
             [compojure.route :as route]
             [lcmap-client.core]
             [lcmap-client.l8.surface-reflectance]
@@ -44,34 +41,4 @@
 (defroutes v2
   management
   (route/not-found "Resource not found"))
-
-(defn get-api-version [version default]
-  (cond
-    (and (>= version 0.0) (< version 1.0)) #'v0
-    (and (>= version 1.0) (< version 2.0)) #'v1
-    (and (>= version 2.0) (< version 3.0)) #'v2
-    :else default))
-
-(defn version-handler
-  "This is a custom handler "
-  [default-api]
-  (fn [request]
-    (let [headers (:headers request)
-          ;; This next line is nuts and took a while to figure out -- results
-          ;; are rendered in log files as symbols, but (headers 'accept) and
-          ;; ('accept headers) didn't work. After an inordinate amount of trial
-          ;; and error, it was discovered that the header keys are actually in
-          ;; lower-case strings at this point in the middleware chain.
-          accept (headers "accept")
-          {version :version} (util/parse-accept-version accept)
-          api (get-api-version version default-api)]
-      (response/render (api request) request))))
-
-(defroutes app
-  (-> default-api-version
-      version-handler
-      ;; XXX once we support SSL, api-defaults needs to be changed to
-      ;; secure-api-defaults
-      (wrap-defaults api-defaults)
-      (wrap-json-response)))
 
