cbf830770ecfd0ca83a9da42139015357a69d061
Duncan M. McGreggor <oubiwann@gmail.com>
2015-10-14 08:44:17 -0500

    Removed constant and updated variable names.

--- src/lcmap_rest/routes.clj
+++ src/lcmap_rest/routes.clj
@@ -8,18 +8,14 @@
             [lcmap-rest.management :as management]
             [lcmap-rest.util :as util]))
 
-(declare v0)
-
-(def default-api-version #'v0)
-
 (defroutes surface-reflectance
   (context lcmap-client.l8.surface-reflectance/context []
-    (GET "/" req
-      (l8-sr/get-resources (:uri req)))
-    (GET "/tiles" [point extent time band :as req]
-      (l8-sr/get-tiles point extent time band req))
-    (GET "/rod" [point time band :as req]
-      (l8-sr/get-rod point time band req))))
+    (GET "/" request
+      (l8-sr/get-resources (:uri request)))
+    (GET "/tiles" [point extent time band :as request]
+      (l8-sr/get-tiles point extent time band request))
+    (GET "/rod" [point time band :as request]
+      (l8-sr/get-rod point time band request))))
 
 ;; XXX this needs to go into a protected area; see this ticket:
 ;;  https://my.usgs.gov/jira/browse/LCMAP-71
