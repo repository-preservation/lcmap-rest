344c77f0cbb4a0ccbb68faed22882022bcc66fd4
Duncan M. McGreggor <oubiwann@gmail.com>
2015-10-14 08:43:34 -0500

    Added parsing defaults.

--- src/lcmap_rest/util.clj
+++ src/lcmap_rest/util.clj
@@ -1,20 +1,23 @@
-(ns lcmap-rest.util)
+(ns lcmap-rest.util
+  (:require [clojure.tools.logging :as log]))
 
 (def accept-regex (re-pattern #"([^;]+)\s*(?:;q=([0-9+\.]+))?\s*(;.+)*"))
 
-(defn parse-accept [str]
+(defn parse-accept [string]
   "Parse a single accept string into a map"
-  ; according to RFC2616, the "q" parameter must precede the accept-extension
-  (let [[_ mr q ae] (re-find accept-regex str)]
+  ;; according to RFC2616, the "q" parameter must precede the accept-extension
+  (let [[_ mr q ae] (re-find accept-regex string)]
     {:media-range mr
      :quality  (java.lang.Double. (or q "1"))
      :accept-extension ae}))
 
 (def accept-version-regex (re-pattern #"application/vnd\.(usgs\.lcmap.)(v(\d(\.\d)?))\+([^;]+)"))
 
-(defn parse-accept-version [str]
-  (let [[_ vend str-vers vers _ ct] (re-find accept-version-regex str)]
-    {:vendor vend
-     :string-version str-vers
+(defn parse-accept-version [string]
+  (log/info (str "Parsing: " string))
+  (let [string (or string "")
+        [_ vend str-vers vers _ ct] (re-find accept-version-regex string)]
+    {:vendor (or vend "NoVendor")
+     :string-version (or str-vers "v0.0")
      :version  (java.lang.Double. (or vers "1"))
-     :content-type ct}))
+     :content-type (or ct "")}))
