60cdd6c8dbe316e2b30030786e2f9c7ece8a2ed3
Duncan McGreggor <oubiwann@gmail.com>
2015-10-22 14:06:37 -0500

    Fixed namespace.

--- src/lcmap_rest/l8.clj
+++ src/lcmap_rest/l8.clj
@@ -1,6 +1,6 @@
 ;;;; This module is for Landsat8-specific code independent of both the routes
 ;;;; and main modules, providing a means of averting cyclic dependencies.
-(ns lcmap-rest.l8.core
+(ns lcmap-rest.l8
   (:require [clojure.tools.logging :as log]))
 
 (def noop :noop)
