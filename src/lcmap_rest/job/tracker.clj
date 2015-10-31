cd2af6c66481bd242158c87798017bd3bebec9ed
Duncan McGreggor <oubiwann@gmail.com>
2015-10-30 22:13:20 -0500

    Significant clean-up to how data is passed and parameters are parsed.

--- src/lcmap_rest/job/tracker.clj
+++ src/lcmap_rest/job/tracker.clj
@@ -2,7 +2,11 @@
   (:require [clojure.tools.logging :as log]
             [clojure.core.match :refer [match]]
             [co.paralleluniverse.pulsar.core :as pulsar]
-            [co.paralleluniverse.pulsar.actors :as actors])
+            [co.paralleluniverse.pulsar.actors :as actors]
+            [clojurewerkz.cassaforte.client :as cc]
+            [clojurewerkz.cassaforte.cql :as cql]
+            [clojurewerkz.cassaforte.query :as query]
+            [lcmap-rest.job.db :as db])
   (:refer-clojure :exclude [promise await bean])
   (:import [co.paralleluniverse.common.util Debug]
            [co.paralleluniverse.actors LocalActor]
@@ -10,102 +14,93 @@
 
 (declare dispatch-handler)
 
-(defn job-result-exists? [args-or-hash]
+(defn job-result-exists? [db-conn args-or-hash]
   ;; XXX query the database, if the result exists, return the data,
   ;; else return false
   false)
 
 (pulsar/defsfn init-job-track
-  [job-id service func-args]
+  [{job-id :job-id db-conn :db-conn default-row :default-row service :service
+   func-args :result :as args}]
   (log/debug "Starting job tracking ...")
   ;; XXX check to see if result already exists
   ;; (result-exists? ...)
-  (let [exists? (job-result-exists? :fix-me!)]
+  (let [exists? (job-result-exists? db-conn job-id)]
     (if exists?
-      (actors/notify! job-id
-                      service
-                      {:type :job-result-exists
-                       :result func-args
-                       :service service})))
-      ;; XXX calculate hash for job and populate job tracking table with
-      ;; currently known data and a status of "pending"
-      (actors/notify! job-id
-                      service
-                      {:type :job-run
-                       :result func-args
-                       :service service}))
+      (actors/notify! service
+                      (into args {:type :job-result-exists}))
+      (do
+        @(db/insert-default db-conn job-id default-row)
+        (actors/notify! service
+                        (into args {:type :job-run}))))))
 
 (pulsar/defsfn return-existing-result
-  [job-id service result]
+  [{service :service :as args}]
   (log/debug "Returning ID for existing job results ...")
   ;; XXX get job ID to the function that needs it ... still need to give this
   ;; some thought
-  (actors/notify! job-id
-                  service
-                  {:type :done
-                   :result result
-                   :service service}))
+  (actors/notify! service
+                  (into args {:type :done})))
 
 (pulsar/defsfn run-job
-  [job-id service [job-func job-args]]
+  [{service :service [job-func job-args] :result :as args}]
   (log/debug (format "Running the job with args %s ..." job-args))
-  (let [job-data @(job-func job-args)]
+  (let [job-data (job-func job-args)]
     (log/debug "Finished job.")
-    (actors/notify! job-id
-                    service
-                    {:type :job-save-data
-                     :result job-data
-                     :service service})))
+    (actors/notify! service
+                    (into args {:type :job-save-data
+                                :result job-data}))))
 
 (pulsar/defsfn save-job-data
-  [job-id service job-output]
-  ;; XXX update results data store
-  (log/debug (str "Saved job data <" job-output "> with <insert id>."))
-  ;; XXX the update returns an id that you can use to refer to this
-  ;; job -- pass that on
-  (actors/notify! job-id
-                  service
-                  {:type :job-track-finish
-                   :result "<insert id>"
-                   :service service}))
+  [{job-id :job-id db-conn :db-conn result-table :result-table service :service
+    job-output :result :as args}]
+  (log/debug (format "Saving job data \n%s with id %s to %s ..."
+                     job-output
+                     job-id
+                     result-table))
+  @(cql/insert-async db-conn result-table {:result_id job-id :result job-output})
+  (log/debug "Saved.")
+  (actors/notify! service
+                  (into args {:type :job-track-finish})))
 
 (pulsar/defsfn finish-job-track
-  [job-id service result]
+  [{job-id :job-id db-conn :db-conn service :service result :result :as args}]
   ;; XXX update tracking data with information on completed job
+  @(db/update-status db-conn job-id 200)
   (log/debug (str "Updated job traking data with " result))
-  (actors/notify! job-id
-                  service
-                  {:type :done
-                   :result result
-                   :service service}))
+  (actors/notify! service
+                  (into args {:type :done})))
 
 (pulsar/defsfn done
-  [job-id service result]
+  [{job-id :job-id :as args}]
   ;; XXX Perform any needed cleanup
-  (log/debug "Finished job tracking.")
+  (log/debug "Finished tracking for job %s." job-id)
   ;;(actors/remove-handler! service #'dispatch-handler)
   ;;(actors/shutdown! event-server)
   ;;(log/debug "Removed event handler.")
   )
 
 (pulsar/defsfn dispatch-handler
-  [{type :type job-id :job-id result :result service :service}]
+  [{type :type :as args}]
   (match [type]
-    [:job-track-init] (init-job-track job-id service result)
-    [:job-result-exists] (return-existing-result job-id service result)
-    [:job-run] (run-job job-id service result)
-    [:job-save-data] (save-job-data job-id service result)
-    [:job-track-finish] (finish-job-track job-id service result)
-    [:done] (done job-id service result)))
+    [:job-track-init] (init-job-track args)
+    [:job-result-exists] (return-existing-result args)
+    [:job-run] (run-job args)
+    [:job-save-data] (save-job-data args)
+    [:job-track-finish] (finish-job-track args)
+    [:done] (done args)))
 
 (defn track-job
-  [job-id func-args]
+  [job-id db-conn default-row result-table func-args]
   (log/debug "Creating event server ...")
   (let [event-server (actors/spawn (actors/gen-event))]
     (actors/add-handler! event-server #'dispatch-handler)
     (log/debug "Added event handler.")
-    (actors/notify! job-id
-                    event-server
+    (actors/notify! event-server
                     {:type :job-track-init
+                     :job-id job-id
+                     :db-conn db-conn
+                     :default-row default-row
+                     :result-table result-table
                      :result func-args
                      :service event-server})))
