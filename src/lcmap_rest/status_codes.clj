9c7e89ff01d5608e8b940b66934ebf4410bd4b42
Duncan McGreggor <oubiwann@gmail.com>
2015-11-02 17:18:47 -0600

    Added status predicates for use in guards.

--- src/lcmap_rest/status_codes.clj
+++ src/lcmap_rest/status_codes.clj
@@ -5,3 +5,9 @@
 (def pending-link 307)
 (def permanant-link 308)
 (def no-resource 404)
+
+(defn ok? [status] (= status ok))
+(defn pending? [status] (= status pending))
+(defn pending-link? [status] (= status pending-link))
+(defn permanant-link? [status] (= status permanant-link))
+(defn no-resource? [status] (= status no-resource))
