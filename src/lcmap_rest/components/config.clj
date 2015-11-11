bcd79bba8af88ae5648707e5e5bf3f800be6dfc6
Duncan McGreggor <dmcgreggor@usgs.gov>
2015-11-11 08:50:39 -0600

    Started adding top-level code comments.

--- src/lcmap_rest/components/config.clj
+++ src/lcmap_rest/components/config.clj
@@ -1,3 +1,6 @@
+;;;; Event LCMAP REST Service system component
+;;;; For more information, see the module-level code comments in
+;;;; lcmap-rest.components.system.
 (ns lcmap-rest.components.config
   (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
--- src/lcmap_rest/components/system.clj
+++ src/lcmap_rest/components/system.clj
@@ -1,3 +1,26 @@
+;;;; Top-level LCMAP REST Service system component
+;;;;
+;;;; Large applications often consist of many stateful processes which must be
+;;;; started and stopped in a particular order. The component model makes
+;;;; those relationships explicit and declarative, instead of implicit in
+;;;; imperative code. The LCMAP REST service project is one such application
+;;;; and early on in its existence it was refactored to support the
+;;;; component/dependency-injection approach.
+;;;;
+;;;; The approach taken in the LCMAP REST application is the following:
+;;;;  * The primary entry point starts the top-level system map (this is
+;;;;    done in lcmap-rest.app.main).
+;;;;  * The top-level system map is defined in
+;;;;    lcmap-rest.components.system.init -- this is what is started in
+;;;;    the main function.
+;;;;  * init takes any parameters not defined in configuration (e.g., the
+;;;;    top-level Ring handler) and instantiates each component while at
+;;;;    the same time defining each components dependencies.
+;;;;  * During start-up and shut-down
+;;;;
+;;;; For more information on the Clojure component library, see:
+;;;;  * https://github.com/stuartsierra/component
+;;;;  * https://www.youtube.com/watch?v=13cmHf_kt-Q
 (ns lcmap-rest.components.system
   (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
