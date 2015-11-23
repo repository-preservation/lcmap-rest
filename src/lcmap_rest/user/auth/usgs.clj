48de165415a33dad2a85eb5ca2c6601a931a740b
Duncan McGreggor <dmcgreggor@usgs.gov>
2015-11-23 13:46:09 -0600

    Changed based on new info from dev instance of ERS.

--- src/lcmap_rest/user/auth/usgs.clj
+++ src/lcmap_rest/user/auth/usgs.clj
@@ -1,11 +1,13 @@
 (ns lcmap-rest.user.auth.usgs
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
             [slingshot.slingshot :refer [throw+]]
             [dire.core :refer [with-handler!]]
             [ring.util.response :as ring]
             [clj-http.client :as http]
             [lcmap-rest.exceptions :as exceptions]
-            [lcmap-rest.status-codes :as status]))
+            [lcmap-rest.status-codes :as status]
+            [lcmap-rest.util :as util]))
 
 ;; XXX uncomment the next line when the service is ready
 ;;(def api-url "https://ers.cr.usgs.gov/api")
@@ -13,6 +15,12 @@
 (def api-url "http://localhost:8888")
 (def auth-url (str api-url "/auth"))
 (def user-url (str api-url "/me"))
+
+;; USGS EROS status codes for the ERS authentication API
+(def success 10)
+(def auth-error 20)
+(def generic-error 30)
+(def input-error 31)
 
 (defn- post-auth  [username password]
   "Post to the USGS auth service and return the auth code."
@@ -30,18 +38,27 @@
     (log/debugf "Got user data for token %s" token)
     (get-in results [:body :data])))
 
+(defn check-status [ers-status errors]
+  (condp #'util/in? ers-status
+    [auth-error input-error generic-error]
+      (throw (exception/Auth-Error (string/join "; " errors)))))
+
 (defn login [username password]
   (let [results (post-auth username password)
-        token (get-in results [:body :data :authToken])
-        user-data (get-user-data token)]
-    (log/infof "User %s successfully authenticated with token %s"
-               username
-               token)
-    ;; XXX save user data in db
-    {:user-id (:contact_id user-data)
-     :username (:username user-data)
-     :email (:email user-data)
-     :token token}))
+        ers-status (get-in results [:body :status])
+        errors (get-in results [:body :errors])
+        token (get-in results [:body :data :authToken])]
+    (check-status ers-status errors)
+    (let [user-data (get-user-data token)]
+      (log/infof "User %s successfully authenticated with token %s"
+                 username
+                 token)
+      ;; XXX save user data in db
+      {:user-id (:contact_id user-data)
+       :username (:username user-data)
+       :roles (:roles user-data)
+       :email (:email user-data)
+       :token token}))
 
 ;;; Login exception handling
 
@@ -55,7 +72,7 @@
 (with-handler! #'login
   [:type 'Auth-Error]
   (fn [e & args]
-    (println "Trouble logging in?")))
+    (log/error e)))
 
 ;; HTTP error status codes returned as exceptions from clj-http
 (with-handler! #'login
