12de744dbaba2cd69bde7f3822cfa31f162f542b
Duncan McGreggor <oubiwann@gmail.com>
2015-11-23 17:01:15 -0600

    Updated test auth server's deps.

--- test/support/auth-server/project.clj
+++ test/support/auth-server/project.clj
@@ -3,7 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.17"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-devel "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring.middleware.logger "0.5.0"]
+                 [com.cemerick/friend "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
   :main ^:skip-aot auth-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
