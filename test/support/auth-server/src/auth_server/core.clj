08d73cceb467766519dad835318d7768b40c2712
Duncan McGreggor <oubiwann@gmail.com>
2015-11-23 17:02:16 -0600

    Added implementation for test auth server.

--- test/support/auth-server/src/auth_server/core.clj
+++ test/support/auth-server/src/auth_server/core.clj
@@ -1,7 +1,74 @@
 (ns auth-server.core
+  (:require [clojure.tools.logging :as log]
+            [org.httpkit.server :as httpkit]
+            [compojure.core :refer [GET POST defroutes]]
+            [compojure.route :as route]
+            [ring.util.response :as ring]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.logger :as logger])
   (:gen-class))
 
+(def good-token "3efc6475b5034309af00549a77b7a6e3")
+
+(defn do-no-username []
+  (ring/response {:data nil
+                  :errors ["Username is required"]
+                  :status 31}))
+
+(defn do-no-password []
+  (ring/response {:data nil
+                  :errors ["Password is required"]
+                  :status 31}))
+
+(defn do-bad-creds []
+  (ring/response {:data nil
+                  :errors ["Invalid username/password"]
+                  :status 20}))
+
+(defn do-good-auth []
+  (ring/response {:data {:authToken good-token}
+                  :status 10}))
+
+(defn do-bad-token []
+  ;; XXX find out what the HTTP response is for a bad/expired token
+  (ring/response {}))
+
+(defn do-good-token []
+  (ring/response {:data {:affiliation "U.S. Federal Government"
+                         :agency "Geological Survey (USGS)"
+                         :contact_id "001010111"
+                         :email "alice@usgs.gov"
+                         :roles ["RPUBLIC" "LANDSAT8CUST"]
+                         :username "alice"}
+                  :status 10}))
+
+(defn do-user-data [headers]
+  (log/debug "Headers:" headers)
+  (log/debug "Passed token:" (headers "x-authtoken"))
+  (log/debug "Good token:" good-token)
+  (if (not= (headers "x-authtoken") good-token)
+    (do-bad-token)
+    (do-good-token)))
+
+(defroutes v1
+  (POST "/api/auth" [username password :as request]
+    (cond
+      (nil? username) (do-no-username)
+      (nil? password) (do-no-password)
+      (and (= username "alice") (= password "secret")) (do-good-auth)
+      :else (do-bad-creds)))
+  (GET "/api/me" request
+    (do-user-data (:headers request))))
+
+(defroutes app
+  (-> v1
+      (wrap-defaults api-defaults)
+      (wrap-json-response)
+      (logger/wrap-with-logger)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (log/info "Starting Compojure server ...")
+  (httpkit/run-server #'app {:port 8888}))
+
