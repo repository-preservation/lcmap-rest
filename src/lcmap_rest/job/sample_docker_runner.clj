322047f57dbac8168595e997d89dee480a2a9acf
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-01-11 17:28:26 -0600

    Updated to use entrypoint parameters.

--- src/lcmap_rest/job/sample_docker_runner.clj
+++ src/lcmap_rest/job/sample_docker_runner.clj
@@ -3,16 +3,16 @@
             [clj-commons-exec :as exec]
             [lcmap-rest.job.tracker :as jt]))
 
-(defn exec-docker-run [[job-id docker-tag docker-args]]
+(defn exec-docker-run [[job-id docker-tag year]]
   (log/debugf "\n\nRunning job %s (executing docker tag %s) ...\n"
                      job-id
                      docker-tag)
-  (:out @(exec/sh ["docker run -e LCMAP_MODEL_ARGS=" docker-args " -t " docker-tag])))
+  (:out @(exec/sh ["docker run -t " docker-tag "--year " year])))
 
-(defn run-model [conn eventd job-id default-row result-table docker-tag docker-args]
+(defn run-model [conn eventd job-id default-row result-table docker-tag year]
   ;; Define some vars for pedagogical clarity
   (let [func #'exec-docker-run
-        args [job-id docker-tag docker-args]]
+        args [job-id docker-tag year]]
     (jt/track-job conn
                   eventd
                   job-id
