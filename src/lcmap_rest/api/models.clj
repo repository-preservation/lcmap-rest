ab020c501c4658624f694edab47e2007d2c37b11
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-01-12 13:37:33 -0600

    Tweaks and additions to get Docker sample model in fighting shape.

--- src/lcmap_rest/api/models.clj
+++ src/lcmap_rest/api/models.clj
@@ -6,6 +6,7 @@
             [lcmap-client.models]
             [lcmap-client.status-codes :as status]
             [lcmap-rest.api.models.ccdc]
+            [lcmap-rest.api.models.sample-docker-process]
             [lcmap-rest.api.models.sample-os-process]))
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -22,6 +23,7 @@
     (GET "/" request
       (get-resources (:uri request))))
   lcmap-rest.api.models.ccdc/routes
+  lcmap-rest.api.models.sample-docker-process/routes
   lcmap-rest.api.models.sample-os-process/routes)
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- src/lcmap_rest/api/models/sample_docker_process.clj
+++ src/lcmap_rest/api/models/sample_docker_process.clj
@@ -3,10 +3,10 @@
             [clojure.core.match :refer [match]]
             [compojure.core :refer [GET HEAD POST PUT context defroutes]]
             [ring.util.response :as ring]
-            [lcmap-rest.api.jobs.sample-os-process :refer [get-result-path
-                                                           get-job-result
-                                                           result-table]]
-            [lcmap-client.models.sample-os-process]
+            [lcmap-rest.api.jobs.sample-docker-process :refer [get-result-path
+                                                               get-job-result
+                                                               result-table]]
+            [lcmap-client.models.sample-docker-process]
             [lcmap-client.status-codes :as status]
             [lcmap-rest.components.httpd :as httpd]
             [lcmap-rest.job.db :as db]
@@ -20,12 +20,13 @@
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn run-model [db eventd seconds year]
+(defn run-model [db eventd docker-tag year]
   ;; generate job-id from hash of args
   ;; return status code 200 with body that has link to where sample result will
   ;; be
-  (log/debugf "run-model got args: [%s %s]" seconds year)
-  (let [job-id (util/get-args-hash science-model-name seconds year)
+  (log/debugf "run-model got args: [%s %s]" docker-tag year)
+  ;; XXX add arg names in hash function to reduce possibility of collision
+  (let [job-id (util/get-args-hash science-model-name docker-tag year)
         default-row {:science_model_name science-model-name
                      :result_keyspace result-keyspace
                      :result_table result-table
@@ -38,7 +39,7 @@
                              job-id
                              default-row
                              result-table
-                             seconds
+                             docker-tag
                              year)
     (log/debug "Called sample-runner ...")
     (ring/status
@@ -50,12 +51,12 @@
 ;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defroutes routes
-  (context lcmap-client.models.sample-os-process/context []
-    (POST "/" [token delay year :as request]
+  (context lcmap-client.models.sample-docker-process/context []
+    (POST "/" [token docker-tag year :as request]
       ;;(log/debug "Request data keys in routes:" (keys request))
       (run-model (httpd/jobdb-key request)
                  (httpd/eventd-key request)
-                 delay
+                 docker-tag
                  year))
     (GET "/:job-id" [job-id :as request]
       (get-job-result (httpd/jobdb-key request) job-id))))
