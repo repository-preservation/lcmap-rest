588fe8caa813e0784cc9124683b29952e73e6d19
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-01-21 10:12:58 -0600

    Added initial serializer code.
    This will be updated to use clj-time so that we get the properly formatted
    string...

--- src/lcmap_rest/serializer.clj
+++ src/lcmap_rest/serializer.clj
@@ -1,3 +1,8 @@
 (ns lcmap-rest.serializer
   (:require [clojure.data.json :as json]))
 
+(defn- serialize-java-util-date [x #^JSONWriter out]
+    (json-str (.toString x)))
+
+(extend java.util.Date json/JSONWriter
+    {:-write serialize-date})
