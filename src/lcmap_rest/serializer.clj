03c680ea17d8feb46fc1c8fa7421a3f117410061
Duncan McGreggor <oubiwann@gmail.com>
2016-01-21 10:43:53 -0600

    Fixed up with Java imports, etc.

--- src/lcmap_rest/serializer.clj
+++ src/lcmap_rest/serializer.clj
@@ -1,8 +1,12 @@
 (ns lcmap-rest.serializer
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json]
+            [clj-time.core :as time])
+  (:import (java.io PrintWriter
+             StringWriter
+             StringReader)))
 
-(defn- serialize-java-util-date [x #^JSONWriter out]
-    (json-str (.toString x)))
+(defn- serialize-java-util-date [x #^StringWriter out]
+    (json/json-str (.toString x)))
 
 (extend java.util.Date json/JSONWriter
-    {:-write serialize-date})
+    {:-write serialize-java-util-date})
