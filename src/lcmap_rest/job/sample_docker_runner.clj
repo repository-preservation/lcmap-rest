4d3d19919b2d5bf7528f377aa11727daf2aeef0b
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-02-01 17:47:06 -0600

    Updated sample runner with the same fixes.

--- src/lcmap_rest/job/sample_docker_runner.clj
+++ src/lcmap_rest/job/sample_docker_runner.clj
@@ -7,7 +7,14 @@
   (log/debugf "\n\nRunning job %s (executing docker tag %s) ...\n"
                      job-id
                      docker-tag)
-  (:out @(exec/sh ["docker run -t " docker-tag "--year " year])))
+  (let [cmd ["/usr/bin/sudo" "/usr/bin/docker"
+             "run" "-t" docker-tag
+             "--year" year]
+        result @(exec/sh cmd)]
+    (case (:exit result)
+      0 (:out result)
+      1 (:err result)
+      [:error "unexpected output" result])))
 
 (defn run-model [conn eventd job-id default-row result-table docker-tag year]
   ;; Define some vars for pedagogical clarity
