cddb2a420493ef9191fb965fa3289d5f5ee6e411
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-02-19 15:07:46 -0600

    This adds support for running CCDC from the REST server.
    This closes a series of tickets we've had open to integrate CCDC and REST into
    the prototype. More work needs to be done, but this is the final keystone that
    lets everything drop into place.

--- src/lcmap/rest/api/models.clj
+++ src/lcmap/rest/api/models.clj
@@ -7,6 +7,7 @@
             [lcmap.client.status-codes :as status]
             [lcmap.rest.api.models.ccdc]
             [lcmap.rest.api.models.ccdc-docker-process]
+            [lcmap.rest.api.models.ccdc-piped-processes]
             [lcmap.rest.api.models.sample-docker-process]
             [lcmap.rest.api.models.sample-os-process]
             [lcmap.rest.api.models.sample-piped-processes]))
@@ -26,6 +27,7 @@
       (get-resources (:uri request))))
   lcmap.rest.api.models.ccdc/routes
   lcmap.rest.api.models.ccdc-docker-process/routes
+  lcmap.rest.api.models.ccdc-piped-processes/routes
   lcmap.rest.api.models.sample-docker-process/routes
   lcmap.rest.api.models.sample-os-process/routes
   lcmap.rest.api.models.sample-piped-processes/routes)
