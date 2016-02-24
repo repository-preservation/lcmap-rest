3a8a584572d34c47386ab9109c86d706389a99c1
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-02-23 19:40:44 -0600

    Updated to latest twig.

--- test/support/auth-server/project.clj
+++ test/support/auth-server/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [twig "0.1.0"]
+                 [twig "0.1.6"]
                  [compojure "1.4.0"]
                  [http-kit "2.1.17"]
                  [ring/ring-jetty-adapter "1.4.0"]
