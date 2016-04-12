dba64c1d61c82aec327a8a2f94d2d00a21ed0a0d
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-04-12 15:18:16 -0500

    Added instrumentation middleware and all-stats resource.

--- src/lcmap/rest/middleware.clj
+++ src/lcmap/rest/middleware.clj
@@ -3,6 +3,9 @@
             [clojure.string :as string]
             [clojure.tools.logging :as log]
             [clojusc.ring.xml :as ring-xml]
+            [metrics.ring.expose :as ring-metrics]
+            [metrics.ring.instrument :as ring-instrument]
+            [lcmap.client.system]
             [lcmap.rest.api.routes :as routes]
             [lcmap.rest.middleware.content-type :as content-type]
             [lcmap.rest.middleware.versioned-api :as versioned-api]))
@@ -10,10 +13,10 @@
 (defn lcmap-handlers
   "This function provides the LCMAP REST server with the single means by which
   the application pulls in all LCMAP Ring handers. Any new handlers that are
-  created should be chained here and not in ``lcmap.rest.app``.
-
-  One of the handlers called in this function is an aggregating handler which
-  consolidates "
+  created should be chained here and not in ``lcmap.rest.app``."
   [default-version]
   (-> (versioned-api/handler default-version)
-      (content-type/handler default-version)))
+      (content-type/handler default-version)
+      (ring-metrics/expose-metrics-as-json
+        (str lcmap.client.system/context "/metrics"))
+      (ring-instrument/instrument)))
