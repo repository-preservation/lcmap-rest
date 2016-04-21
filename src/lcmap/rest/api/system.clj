d40e594d493879dca1bdfdae0dc8313fa76628da
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-04-21 14:25:52 -0500

    Updated system routes.

--- src/lcmap/rest/api/system.clj
+++ src/lcmap/rest/api/system.clj
@@ -7,6 +7,7 @@
             [lcmap.client.system]
             [lcmap.rest.serializer :as serializer]
             [lcmap.rest.system :as system]
+            [lcmap.rest.errors :as errors]
             [lcmap.rest.middleware.http-util :as http]))
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -19,35 +20,48 @@
 ;; XXX this needs to be protected; see this ticket:
 ;;   https://my.usgs.gov/jira/browse/LCMAP-71
 (defroutes routes
+  ;; Top-level system routes
   (context lcmap.client.system/context []
     (GET "/" request
       (get-resources (:uri request)))
-    ;; Note that lcmap.rest.middleware sets up a route for
-    ;; /api/system/metrics/all
-    (GET "/metrics/names" []
-      (http/response :result
-        (system/get-metrics-names metrics/default-registry)))
-    (GET "/metrics/counters" []
-      (http/response :result
-        (system/get-metrics-counters metrics/default-registry)))
-    (GET "/metrics/gauges" []
-      (http/response :result
-        (system/get-metrics-gauges metrics/default-registry)))
-    (GET "/metrics/histograms" []
-      (http/response :result
-        (system/get-metrics-histograms metrics/default-registry)))
-    (GET "/metrics/meters" []
-      (http/response :result
-        (system/get-metrics-meters metrics/default-registry)))
-    (GET "/metrics/timers" []
-      (http/response :result
-        (system/get-metrics-timers metrics/default-registry)))
     ;; XXX add more management resources
     (GET "/status" []
       (system/get-xml-status))
     ;; XXX json-status is a hack until we get dynamic content type support
     (GET "/json-status" []
-      (system/get-json-status))))
+      (system/get-json-status)))
+  ;; system/metrics routes
+  (context lcmap.client.system/metrics []
+    ;; Note that lcmap.rest.middleware sets up a route for
+    ;; /api/system/metrics/all
+    (GET "/names" []
+      (http/response :result
+        (system/get-metrics-names metrics/default-registry)))
+    (GET "/counters" []
+      (http/response :result
+        (system/get-metrics-counters metrics/default-registry)))
+    (GET "/gauges" []
+      (http/response :result
+        (system/get-metrics-gauges metrics/default-registry)))
+    (GET "/histograms" []
+      (http/response :result
+        (system/get-metrics-histograms metrics/default-registry)))
+    (GET "/meters" []
+      (http/response :result
+        (system/get-metrics-meters metrics/default-registry)))
+    (GET "/timers" []
+      (http/response :result
+        (system/get-metrics-timers metrics/default-registry))))
+  ;; system/reference routes
+  (context lcmap.client.system/reference []
+    (GET "/error-types" []
+      (http/response :result errors/category))
+    (GET "/error-type/:id" []
+      (http/response :result (id errors/category)))
+    (GET "/errors" []
+      (http/response :result errors/lookup))
+    (GET "/error/:id" []
+      (http/response :result (id errors/lookup)))))
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
