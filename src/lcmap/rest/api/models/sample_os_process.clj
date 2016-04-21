588aae30b76ec3ce4c6f8b5fad1a741fd04f1e98
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-04-21 15:40:01 -0500

    Use try/except as a sanity check.
    Also did some code clean-up.

--- src/lcmap/rest/api/models/sample_os_process.clj
+++ src/lcmap/rest/api/models/sample_os_process.clj
@@ -11,7 +11,7 @@
             [lcmap.client.status-codes :as status]
             [lcmap.rest.components.httpd :as httpd]
             [lcmap.rest.middleware.http-util :as http]
-            [lcmap.rest.types :refer [StrInt StrYear]]
+            [lcmap.rest.types :refer [Any StrInt StrYear]]
             [lcmap.rest.util :as util]
             [lcmap.see.job.db :as db]
             [lcmap.see.job.sample-runner :as sample-runner]))
@@ -23,36 +23,57 @@
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(schema/defn run-model
+(defn make-default-row
   ""
-  [db
-   eventd
+  [result-id default-status]
+  {:science_model_name science-model-name
+   :result_keyspace result-keyspace
+   :result_table result-table
+   :result_id result-id
+   :status default-status})
+
+;;; Science Model Execution ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(schema/defn run-typed-model
+  ""
+  [db :- Any
+   eventd :- Any
    seconds :- StrInt
    year :- StrYear]
   ;; generate job-id from hash of args
   ;; return status code 200 with body that has link to where sample result will
   ;; be
   (log/debugf "run-model got args: [%s %s]" seconds year)
-  (let [job-id (util/get-args-hash science-model-name
-                                   :delay seconds
-                                   :year year)
-        default-row {:science_model_name science-model-name
-                     :result_keyspace result-keyspace
-                     :result_table result-table
-                     :result_id job-id
-                     :status status/pending}]
+  (let [job-id (util/get-args-hash
+                 science-model-name :delay seconds :year year)]
     ;;(log/debugf "sample model run (job id: %s)" job-id)
     ;;(log/debugf "default row: %s" default-row)
-    (sample-runner/run-model (:conn db)
-                             (:eventd eventd)
-                             job-id
-                             default-row
-                             result-table
-                             seconds
-                             year)
+    (sample-runner/run-model
+      (:conn db)
+      (:eventd eventd)
+      job-id
+      (make-default-row job-id status/pending)
+      result-table
+      seconds
+                       year)
     (log/debug "Called sample-runner ...")
     (http/response :result {:link {:href (get-result-path job-id)}}
                    :status status/pending-link)))
+
+(defn run-model
+  ""
+  [& args]
+  (try
+    (schema/with-fn-validation
+      (apply run-typed-model args))
+    (catch RuntimeException e
+      (let [error (.getMessage e)]
+        (log/error "Got error:" error)
+        (->
+          (http/response :errors [error]
+                         :status status/client-error)
+          ;; update to take mime sub-type from Accept
+          (http/problem-header))))))
 
 ;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -60,18 +81,19 @@
   (context lcmap.client.models.sample-os-process/context []
     (POST "/" [token delay year :as request]
       ;;(log/debug "Request data keys in routes:" (keys request))
-      (schema/with-fn-validation
-        (run-model
-          (httpd/jobdb-key request)
-          (httpd/eventd-key request)
-          delay
-          year)))
+      (run-model
+        ;; XXX choose a good way to pass the Accept header to the model
+        ;;     so that the error problem type header can be set appropriately
+        (httpd/jobdb-key request)
+        (httpd/eventd-key request)
+        delay
+        year))
     (GET "/:job-id" [job-id :as request]
       (get-job-result (httpd/jobdb-key request) job-id))))
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(with-handler! #'schema/with-fn-validation
+(with-handler! #'run-model
   RuntimeException
   (fn [e & args]
     (log/error "error: %s; args: %s" e args)
