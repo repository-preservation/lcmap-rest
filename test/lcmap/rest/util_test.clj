64628424a5b8235e61cdb75780e96b15debcb834
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-05-03 09:50:42 -0500

    Moved test to match changes in project structure.

--- test/lcmap/rest/util_test.clj
+++ test/lcmap/rest/util_test.clj
@@ -2,15 +2,6 @@
   (:require [clojure.test :refer :all]
             [lcmap.rest.util :as util]))
 
-(deftest parse-accept-test
-  (are [a b] (= a b)
-    (util/parse-accept "text/html") {:accept-extension nil
-                                     :media-range "text/html"
-                                     :quality 1.0}
-    (util/parse-accept "text/html;q=0.9") {:media-range "text/html"
-                                           :quality 0.9
-                                           :accept-extension nil}))
-
 (deftest get-args-hash-test
   (are [a b] (= a b)
     (util/get-args-hash "model-name" "a" "b" "c")
