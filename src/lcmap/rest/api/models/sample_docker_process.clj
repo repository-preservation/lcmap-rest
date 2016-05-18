9a348d61c6fa78820cceebe3f7e6b8fa805e9773
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-05-17 20:51:22 -0500

    Updated model to use new general model functions.

--- src/lcmap/rest/api/models/sample_docker_process.clj
+++ src/lcmap/rest/api/models/sample_docker_process.clj
@@ -2,12 +2,14 @@
   (:require [clojure.tools.logging :as log]
             [clojure.core.match :refer [match]]
             [compojure.core :refer [GET HEAD POST PUT context defroutes]]
-            [ring.util.response :as ring]
+            [schema.core :as schema]
             [lcmap.client.models.sample-docker-process]
             [lcmap.client.status-codes :as status]
-            [lcmap.rest.api.jobs :as jobs]
+            [lcmap.rest.api.jobs :as job]
+            [lcmap.rest.api.models.core :as model]
             [lcmap.rest.components.httpd :as httpd]
             [lcmap.rest.middleware.http-util :as http]
+            [lcmap.rest.types :refer [Any Str StrYear]]
             [lcmap.rest.util :as util]
             [lcmap.see.job.db :as db]
             [lcmap.see.model.sample-docker :as sample-docker-runner]))
@@ -16,38 +18,37 @@
 
 (def result-table "samplemodel")
 (def science-model-name "sample docker")
-(def result-keyspace "lcmap")
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TBD
+(defn make-default-row
+  [id]
+  ""
+  (model/make-default-row id result-table science-model-name))
 
 ;;; Science Model Execution ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn run-model [db eventd docker-tag year]
+(schema/defn run-model
+  ""
+  [^Any request
+   ^Str docker-tag
+   ^StrYear year]
+  (log/debugf "run-model got args: [%s %s]" docker-tag year)
   ;; generate job-id from hash of args
   ;; return status code 200 with body that has link to where sample result will
   ;; be
-  (log/debugf "run-model got args: [%s %s]" docker-tag year)
-  (let [job-id (util/get-args-hash science-model-name
-                                   :docker-tag docker-tag
-                                   :year year)
-        default-row {:science_model_name science-model-name
-                     :result_keyspace result-keyspace
-                     :result_table result-table
-                     :result_id job-id
-                     :status status/pending}]
-    ;;(log/debugf "sample model run (job id: %s)" job-id)
-    ;;(log/debugf "default row: %s" default-row)
-    (sample-docker-runner/run-model (:conn db)
-                             (:eventd eventd)
-                             job-id
-                             default-row
-                             result-table
-                             docker-tag
-                             year)
-    (log/debug "Called sample-runner ...")
-    (http/response :result {:link {:href (jobs/get-result-path job-id)}}
+  (let [job-id (util/get-args-hash
+                 science-model-name :docker-tag docker-tag :year year)]
+    (sample-docker-runner/run-model
+      (:conn (httpd/jobdb-key request))
+      (:eventd (httpd/eventd-key request))
+      job-id
+      (make-default-row job-id)
+      result-table
+      docker-tag
+      year)
+    (log/debug "Called sample-docker-runner ...")
+    (http/response :result {:link {:href (job/get-result-path job-id)}}
                    :status status/pending-link)))
 
 ;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -55,13 +56,11 @@
 (defroutes routes
   (context lcmap.client.models.sample-docker-process/context []
     (POST "/" [token docker-tag year :as request]
+      ;; XXX use token to check user/session/authorization
       ;;(log/debug "Request data keys in routes:" (keys request))
-      (run-model (httpd/jobdb-key request)
-                 (httpd/eventd-key request)
-                 docker-tag
-                 year))
+      (model/validate #'run-model request docker-tag year))
     (GET "/:job-id" [job-id :as request]
-      (jobs/get-job-result (httpd/jobdb-key request) job-id))))
+      (job/get-job-result (httpd/jobdb-key request) job-id))))
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
