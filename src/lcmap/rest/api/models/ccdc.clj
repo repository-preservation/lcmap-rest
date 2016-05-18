835a9151d3d18b8d7bf76a2fb1709d68b8c74a96
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-05-17 21:21:26 -0500

    Converted ccdc model and stubbed more out.

--- src/lcmap/rest/api/models/ccdc.clj
+++ src/lcmap/rest/api/models/ccdc.clj
@@ -1,43 +1,89 @@
 (ns lcmap.rest.api.models.ccdc
   (:require [clojure.tools.logging :as log]
             [compojure.core :refer [GET HEAD POST PUT context defroutes]]
+            [schema.core :as schema]
             [lcmap.client.models.ccdc]
             [lcmap.client.status-codes :as status]
-            [lcmap.rest.api.jobs :as jobs]
+            [lcmap.rest.api.jobs :as job]
+            [lcmap.rest.api.models.core :as model]
             [lcmap.rest.components.httpd :as httpd]
+            [lcmap.rest.middleware.http-util :as http]
+            [lcmap.rest.types :refer [Any Str StrBool StrInt StrDate]]
             [lcmap.rest.util :as util]
             [lcmap.see.job.db :as db]
-            [lcmap.see.model.sample :as sample-runner]))
+            ; [lcmap.see.model.ccdc :as ccdc-runner]
+            ))
 
 ;;; Supporting Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def result-table "XXX")
-(def science-model-name "ccdc model")
-(def result-keyspace "lcmap")
+(def result-table "ccdcmodel")
+(def science-model-name "ccdc")
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TBD
+(defn make-default-row
+  [id]
+  ""
+  (model/make-default-row id result-table science-model-name))
 
 ;;; Science Model Execution ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn run-model [db eventd arg1 arg2]
-  (log/debugf "run-model got args: [%s %s]" arg1 arg2)
-  (let [job-id (util/get-args-hash "ccdc" :arg1 arg1 :arg2 arg2)]
-    (str "model run (job id: " job-id ") tbd")))
+(schema/defn run-model
+  ""
+  [^Any request
+   ^Str spectra
+   ^StrInt x-val
+   ^StrInt y-val
+   ^StrDate start-time
+   ^StrDate end-time
+   ^StrInt row
+   ^StrInt col
+   ^Str in-dir
+   ^Str out-dir
+   ^Any scene-list
+   ^StrBool verbose]
+  (log/debugf "run-model got args: %s" [spectra x-val y-val start-time end-time
+                                        row col in-dir out-dir scene-list verbose])
+  ;; generate job-id from hash of args
+  ;; return status code 200 with body that has link to where the ccdc result will
+  ;; be
+  (let [job-id (util/get-args-hash science-model-name
+                                   :spectra spectra
+                                   :x-val x-val
+                                   :y-val y-val
+                                   :start-time start-time
+                                   :end-time end-time
+                                   :row row
+                                   :col col
+                                   :in-dir in-dir
+                                   :out-dir out-dir
+                                   :scene-list scene-list
+                                   :verbose verbose)]
+    (log/warn "Mesos infrastructure not yet in place!")
+    ; (ccdc-runner/run-model
+    ;   (:conn (httpd/jobdb-key request))
+    ;   (:eventd (httpd/eventd-key request))
+    ;   job-id
+    ;   (make-default-row job-id)
+    ;   result-table
+    ;   spectra x-val y-val start-time end-time
+    ;   row col in-dir out-dir scene-list verbose)
+    ;   (log/debug "Called ccdc runner ...")
+    (http/response :result {:link {:href (job/get-result-path job-id)}}
+                   :status status/pending-link)))
 
 ;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defroutes routes
   (context lcmap.client.models.ccdc/context []
-    (POST "/" [arg1 arg2 :as request]
-      ;;(log/debug "Request data keys in routes:" (keys request))
-      (run-model (httpd/jobdb-key request)
-                 (httpd/eventd-key request)
-                 arg1
-                 arg2))
+    (POST "/" [token spectra x-val y-val start-time end-time
+                     row col in-dir out-dir scene-list verbose :as request]
+      (model/validate
+        #'run-model request
+        spectra x-val y-val start-time end-time
+        row col in-dir out-dir scene-list verbose))
     (GET "/:job-id" [job-id :as request]
-      (jobs/get-job-result (httpd/jobdb-key request) job-id))))
+      (job/get-job-result (httpd/jobdb-key request) job-id))))
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
