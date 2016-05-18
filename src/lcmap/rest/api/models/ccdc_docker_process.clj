3fc8d2fc66503a7b522f80459432c1c2c56a7e3d
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-05-17 21:13:00 -0500

    Another model converted.

--- src/lcmap/rest/api/models/ccdc_docker_process.clj
+++ src/lcmap/rest/api/models/ccdc_docker_process.clj
@@ -2,11 +2,14 @@
   (:require [clojure.tools.logging :as log]
             [clojure.core.match :refer [match]]
             [compojure.core :refer [GET HEAD POST PUT context defroutes]]
+            [schema.core :as schema]
             [lcmap.client.models.ccdc-docker-process]
             [lcmap.client.status-codes :as status]
-            [lcmap.rest.api.jobs :as jobs]
+            [lcmap.rest.api.jobs :as job]
+            [lcmap.rest.api.models.core :as model]
             [lcmap.rest.components.httpd :as httpd]
             [lcmap.rest.middleware.http-util :as http]
+            [lcmap.rest.types :refer [Any Str StrBool StrInt StrDate]]
             [lcmap.rest.util :as util]
             [lcmap.see.job.db :as db]
             [lcmap.see.model.ccdc-docker :as ccdc-docker-runner]))
@@ -15,53 +18,72 @@
 
 (def result-table "ccdcmodel")
 (def science-model-name "ccdc")
-(def result-keyspace "lcmap")
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TBD
+(defn make-default-row
+  [id]
+  ""
+  (model/make-default-row id result-table science-model-name))
 
 ;;; Science Model Execution ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn run-model [db eventd row col in-dir out-dir scene-list verbose]
-  (log/debugf "run-model got args: %s" [row col in-dir out-dir scene-list verbose])
+(schema/defn run-model
+  ""
+  [^Any request
+   ^Str spectra
+   ^StrInt x-val
+   ^StrInt y-val
+   ^StrDate start-time
+   ^StrDate end-time
+   ^StrInt row
+   ^StrInt col
+   ^Str in-dir
+   ^Str out-dir
+   ^Any scene-list
+   ^StrBool verbose]
+  (log/debugf "run-model got args: %s" [spectra x-val y-val start-time end-time
+                                        row col in-dir out-dir scene-list verbose])
+  ;; generate job-id from hash of args
+  ;; return status code 200 with body that has link to where the ccdc result will
+  ;; be
   (let [job-id (util/get-args-hash science-model-name
+                                   :spectra spectra
+                                   :x-val x-val
+                                   :y-val y-val
+                                   :start-time start-time
+                                   :end-time end-time
                                    :row row
                                    :col col
                                    :in-dir in-dir
-                                   :out-dir :out-dir
+                                   :out-dir out-dir
                                    :scene-list scene-list
-                                   :verbose verbose)
-        default-row {:science_model_name science-model-name
-                     :result_keyspace result-keyspace
-                     :result_table result-table
-                     :result_id job-id
-                     :status status/pending}]
+                                   :verbose verbose)]
     ;;(log/debugf "ccdc model run (job id: %s)" job-id)
     ;;(log/debugf "default row: %s" default-row)
     (ccdc-docker-runner/run-model
-      (:conn db)
-      (:eventd eventd)
+      (:conn (httpd/jobdb-key request))
+      (:eventd (httpd/eventd-key request))
       job-id
-      default-row
+      (make-default-row job-id)
       result-table
       row col in-dir out-dir scene-list verbose)
     (log/debug "Called ccdc-runner ...")
-    (http/response :result {:link {:href (jobs/get-result-path job-id)}}
+    (http/response :result {:link {:href (job/get-result-path job-id)}}
                    :status status/pending-link)))
 
 ;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defroutes routes
   (context lcmap.client.models.ccdc-docker-process/context []
-    (POST "/" [token row col in-dir out-dir scene-list verbose :as request]
-      ;;(log/debugf "POST request got: %s" request)
-      ;;(log/debug "Request data keys in routes:" (keys request))-
-      (run-model (httpd/jobdb-key request)
-                 (httpd/eventd-key request)
-                 row col in-dir out-dir scene-list verbose))
+    (POST "/" [token spectra x-val y-val start-time end-time
+                     row col in-dir out-dir scene-list verbose :as request]
+      (model/validate
+        #'run-model request
+        spectra x-val y-val start-time end-time
+        row col in-dir out-dir scene-list verbose))
     (GET "/:job-id" [job-id :as request]
-      (jobs/get-job-result (httpd/jobdb-key request) job-id))))
+      (job/get-job-result (httpd/jobdb-key request) job-id))))
 
 ;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
