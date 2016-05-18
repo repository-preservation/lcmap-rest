db1f1a50124d9abb6784e811b6f4c08b3958371d
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-05-17 22:09:53 -0500

    Added lint targets.

--- resources/make/code.mk
+++ resources/make/code.mk
@@ -30,6 +30,16 @@ loc:
 check:
 	@lein with-profile +testing,-dev test
 
+lint:
+	@lein kibit
+	@lein eastwood "{:namespaces [:source-paths]}"
+
+lint-unused:
+	@ lein eastwood "{:linters [:unused-fn-args :unused-locals :unused-namespaces :unused-private-vars :wrong-ns-form] :namespaces [:source-paths]}"
+
+lint-ns:
+	@ lein eastwood "{:linters [:unused-namespaces :wrong-ns-form] :namespaces [:source-paths]}"
+
 run:
 	-@lein trampoline run
 
