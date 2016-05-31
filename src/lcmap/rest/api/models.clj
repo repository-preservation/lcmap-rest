1f094deef0f68e57f555ec8f7cb58ce049eac55e
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-30 23:12:59 -0500

    Add resource for NDVI model

--- src/lcmap/rest/api/models.clj
+++ src/lcmap/rest/api/models.clj
@@ -8,6 +8,7 @@
             [lcmap.rest.api.models.sample-docker-process]
             [lcmap.rest.api.models.sample-os-process]
             [lcmap.rest.api.models.sample-piped-processes]
+            [lcmap.rest.api.models.ndvi]
             [lcmap.rest.middleware.http-util :as http]))
 
 ;;; Supporting Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,6 +34,7 @@
   lcmap.rest.api.models.ccdc/routes
   lcmap.rest.api.models.ccdc-docker-process/routes
   lcmap.rest.api.models.ccdc-piped-processes/routes
+  lcmap.rest.api.models.ndvi/routes
   lcmap.rest.api.models.sample-docker-process/routes
   lcmap.rest.api.models.sample-os-process/routes
   lcmap.rest.api.models.sample-piped-processes/routes)
