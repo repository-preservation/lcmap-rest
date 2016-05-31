c9cf359909acb542e1efdf1a54d18bb3cfc1b1bb
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-30 23:12:59 -0500

    Add resource for NDVI model

---
+++ src/lcmap/rest/api/models/ndvi.clj
@@ -0,0 +1,26 @@
+(ns lcmap.rest.api.models.ndvi
+  (:require [clojure.tools.logging :as log]
+            [compojure.core :refer [GET HEAD POST PUT context defroutes]]
+            [schema.core :as schema]
+            [lcmap.rest.api.jobs :as job]
+            [lcmap.rest.api.models.core :as model]
+            [lcmap.rest.components.httpd :as httpd]
+            [lcmap.rest.middleware.http-util :as http]
+            [lcmap.rest.types :refer [Any Str StrBool StrInt StrDate]]
+            [lcmap.rest.util :as util]
+            [lcmap.see.model.ndvi :as ndvi]))
+
+;;; Routes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defroutes routes
+  (context "/api/models/ndvi" []
+    (POST "/" [token x y t1 t2 :as request]
+      ;; use token somehow
+      ;; track job
+      (http/response :result (ndvi/run "meow-job-id" x y t1 t2)))
+    (GET "/:job-id" [job-id :as request]
+      (job/get-job-result (:component request) job-id))))
+
+;;; Exception Handling ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; XXX TBD
