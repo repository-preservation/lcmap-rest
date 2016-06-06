80a61343389923c3d5afcc863bca93a6467789ff
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-06 09:00:08 -0500

    Remove checkouts for test setup.
    Now that snapshots for dependencies are regularly pushed, it doesn't make sense to pull master branches for dependencies.

--- test/support/setup.sh
+++ test/support/setup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir checkouts
-cd checkouts && git clone https://github.com/USGS-EROS/lcmap-config.git && cd -
-cd checkouts && git clone https://github.com/USGS-EROS/lcmap-client-clj.git && cd -
-cd checkouts && git clone https://github.com/USGS-EROS/lcmap-data.git && cd -
-cd checkouts && git clone https://github.com/USGS-EROS/lcmap-see.git && cd -
-cd checkouts && git clone https://github.com/USGS-EROS/lcmap-event.git && cd -
 mkdir ~/.usgs/
 cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.test.ini
 cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.ini
