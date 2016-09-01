c7a2251580a5ee22c9393283faaf177d6fd42979
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-08-31 20:34:49 -0500

    Added missing installs.

--- test/support/setup_deps.sh
+++ test/support/setup_deps.sh
@@ -10,6 +10,10 @@ cd checkouts && \
     git clone https://github.com/USGS-EROS/lcmap-client-clj.git && \
     cd ../
 
+sudo apt-add-repository ppa:ubuntugis/ppa -y
+sudo apt-get update -qq
+sudo apt-get install libgdal-dev libgdal-java -y
+
 mkdir ~/.usgs/
 cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.test.ini
 cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.ini
