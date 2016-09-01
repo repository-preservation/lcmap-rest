2b085613e18d7670852e65ce622f26b8d3ab2654
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-08-31 20:17:56 -0500

    More travis tweaks.

--- test/support/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir ~/.usgs/
-cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.test.ini
-cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.ini
--- test/support/setup_deps.sh
+++ test/support/setup_deps.sh
@@ -2,9 +2,15 @@
 
 mkdir checkouts
 cd checkouts && \
+    git clone https://github.com/USGS-EROS/lcmap-config.git && \
+    git clone https://github.com/USGS-EROS/lcmap-logger.git && \
+    git clone https://github.com/USGS-EROS/lcmap-event.git && \
+    git clone https://github.com/USGS-EROS/lcmap-data.git && \
+    git clone https://github.com/USGS-EROS/lcmap-see.git && \
     git clone https://github.com/USGS-EROS/lcmap-client-clj.git && \
     cd ../
 
 mkdir ~/.usgs/
-cp test/support/sample_config.ini ~/.usgs/lcmap.ini
+cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.test.ini
+cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.ini
 
