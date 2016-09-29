cc9343c5630dd1152ee2cb4a813db20662a0ad59
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-09-28 23:54:23 -0500

    Removed test for code that's no longer there.

--- test/lcmap/rest/util_test.clj
+++ test/lcmap/rest/util_test.clj
@@ -2,15 +2,6 @@
   (:require [clojure.test :refer :all]
             [lcmap.rest.util :as util]))
 
-(deftest get-args-hash-test
-  (are [a b] (= a b)
-    (util/get-args-hash "model-name" "a" "b" "c")
-    "7e564057bd312ec540d3fbdc208427ba"
-    (util/get-args-hash "model-name" "a" "b" "c" [1 2 3])
-    "83a13779b6a2fd6286335402c94468f4"
-    (util/get-args-hash "model-name" :arg-1 "a" "b" "c" :arg-2 [1 2 3])
-    "643eea8614e1118b4bd24bbdffef0d51"))
-
 (deftest make-bool-test
   (are [a b] (= a b)
     (util/make-bool 0) false
