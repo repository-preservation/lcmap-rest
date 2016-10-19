e1746d57adfe380a8e12cb2c560df753662fe336
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-10-19 18:42:19 -0500

    Updates to use the new Mesos docker model.

--- src/lcmap/rest/api/models/sample_docker.clj
+++ src/lcmap/rest/api/models/sample_docker.clj
@@ -11,16 +11,16 @@
             [lcmap.rest.types :refer [Any Str StrYear]]
             [lcmap.rest.util :as util]
             [lcmap.see.backend :as see]
-            [lcmap.see.backend.native.models.sample-docker]))
+            [lcmap.see.backend.native.models.sample-docker]
+            [lcmap.see.backend.mesos.models.sample-docker]))
 
 ;;; Science Model Execution ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (schema/defn run-model
   ""
   [^Any request
-   ^Str docker-tag
-   ^StrYear year]
-  (log/debugf "run-model got args: [%s %s]" docker-tag year)
+   ^Str docker-tag]
+  (log/debug "run-model got arg:" docker-tag)
   ;; generate job-id from hash of args
   ;; return status code 200 with body that has link to where sample result will
   ;; be
@@ -28,7 +28,7 @@
         backend-impl (get-in component [:see :backend])
         job-id (see/run-model
                  backend-impl
-                 ["sample-docker" docker-tag year])]
+                 ["sample-docker" docker-tag])]
     (log/debugf "Got backend in REST API: %s (%s)" backend-impl (type backend-impl))
     (log/debug "Called sample-docker; got id: " job-id)
     (http/response :result {:link {:href (job/get-result-path job-id)}}
@@ -38,10 +38,10 @@
 
 (defroutes routes
   (context lcmap.client.models.sample-docker/context []
-    (POST "/" [token docker-tag year :as request]
+    (POST "/" [token docker-tag :as request]
       ;; XXX use token to check user/session/authorization
       ;;(log/debug "Request data keys in routes:" (keys request))
-      (model/validate #'run-model request docker-tag year))
+      (model/validate #'run-model request docker-tag))
     (GET "/:job-id" [job-id :as request]
       (job/get-job-result (:component request) job-id))))
 
